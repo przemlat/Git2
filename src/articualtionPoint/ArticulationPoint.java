3692bccbc40f0e019903339303634aa49d5f6e30
Przemek Latoch <Przemyslaw.Latoch@vimn.com>
2018-06-20 08:34:08 +0200

    Giz 2 completed

--- src/articualtionPoint/ArticulationPoint.java
+++ src/articualtionPoint/ArticulationPoint.java
@@ -2,57 +2,90 @@ package articualtionPoint;
 
 import common.Node;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 class ArticulationPoint {
+
+    public static void main(String[] args) throws IOException {
+        FileReader fileReader = new FileReader("text.txt");
+        BufferedReader bufferedReader = new BufferedReader(fileReader);
+        String line = bufferedReader.readLine();
+
+        int lnumber = 0;
+        while (line != null) {
+            lnumber++;
+            line = bufferedReader.readLine();
+        }
+        int nodesNumber = lnumber - 1;
+
+        Node[] nodes = new Node[nodesNumber + 1];
+        int n = 1;
+        while (n <= nodesNumber) {
+            nodes[n] = new Node(n);
+            n++;
+        }
+
+        FileReader newFileReader = new FileReader("text.txt");
+        BufferedReader newBufferedReader = new BufferedReader(newFileReader);
+
+        int index = 0;
+        for (int i = 1; i <= nodesNumber; i++) {
+            index++;
+            String lowNode = newBufferedReader.readLine();
+            for (int j = 0; j < lowNode.length(); j++) {
+                if (String.valueOf(lowNode.charAt(j)).equals(" ")) {
+                    j++;
+                }
+                nodes[index].addNode(nodes[Character.getNumericValue(lowNode.charAt(j))]);
+                nodes[Character.getNumericValue(lowNode.charAt(j))].addNode(nodes[index]);
+            }
+        }
+        ArticulationPoint articulationPoint = new ArticulationPoint(nodes);
+        articulationPoint.getResult();
+    }
 
     private Node[] nodes;
 
-    ArticulationPoint(Node[] nodes) {
+    private ArticulationPoint(Node[] nodes) {
         this.nodes = nodes;
     }
 
-    void getResult() {
-        for (int i = 1; i < nodes.length; i++) {
-            if (!nodes[i].isVisited()) {
-                DFS(1, i, -1);
-            }
-        }
-    }
-
-    private int DFS(int indexDFS, int currentNodeNumber, int parentNode) {
-        Node currentNode = nodes[currentNodeNumber];
-        currentNode.setDfsIndex(indexDFS);
-        currentNode.setLow(indexDFS);
-
-        int test = 0;
-
+    private int DeepFirstSearch(int indexDFS, int nodeIndex, int parentNode) {
+        Node node = nodes[nodeIndex];
+        node.setLow(indexDFS);
+        node.setDfsIndex(indexDFS);
+        boolean condition = false;
         StringBuilder stringBuilder = new StringBuilder();
 
-        for (int i = 0; i < currentNode.getNodes().size(); i++) {
-            Node neighbour = currentNode.getNodes().get(i);
-            if (neighbour.getNodeIndex() != parentNode) {
-                if (!(neighbour.getDfsIndex() > 0)) {
-                    int nestedLow = DFS(indexDFS + 1, neighbour.getNodeIndex(), currentNodeNumber);
-                    if (nestedLow < currentNode.getLow()) {
-                        currentNode.setLow(nestedLow);
+        for (int i = 0; i < node.getNodes().size(); i++) {
+            Node neighbourNode = node.getNodes().get(i);
+            if (neighbourNode.getNodeIndex() != parentNode) {
+                if (neighbourNode.getDfsIndex() <= 0) {
+                    int lowNode = DeepFirstSearch(indexDFS + 1, neighbourNode.getNodeIndex(), nodeIndex);
+                    if (lowNode >= node.getDfsIndex()) {
+                        condition = true;
+                    } else if (lowNode < node.getLow()) {
+                        node.setLow(lowNode);
                     }
-                    if (nestedLow < currentNode.getLow()) {
-                        currentNode.setLow(nestedLow);
-                    }
-                    if (nestedLow >= currentNode.getDfsIndex()) {
-                        test = 1;
-                    }
-
-                } else if (neighbour.getDfsIndex() < currentNode.getLow()) {
-                    currentNode.setLow(neighbour.getDfsIndex());
+                } else if (neighbourNode.getDfsIndex() < node.getLow()) {
+                    node.setLow(neighbourNode.getDfsIndex());
                 }
             }
         }
-
-        if (test == 1) {
-            stringBuilder.append(currentNodeNumber).append(" ");
+        if (condition) {
+            stringBuilder.append(nodeIndex).append(" ");
         }
-
         System.out.print(stringBuilder.toString());
-        return currentNode.getLow();
+        return node.getLow();
+    }
+
+    private void getResult() {
+        for (int i = 1; i < nodes.length; i++) {
+            if (!nodes[i].isVisited()) {
+                DeepFirstSearch(1, i, -1);
+            }
+        }
     }
 }
--- src/bridge/BridgeFinder.java
+++ src/bridge/BridgeFinder.java
@@ -2,46 +2,88 @@ package bridge;
 
 import common.Node;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 class BridgeFinder {
+
+    private static StringBuilder bridges = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        FileReader fileReader = new FileReader("text.txt");
+        BufferedReader bufferedReader = new BufferedReader(fileReader);
+        String line = bufferedReader.readLine();
+
+        int lnumber = 0;
+        while (line != null) {
+            lnumber++;
+            line = bufferedReader.readLine();
+        }
+        int nodesNumber = lnumber - 1;
+
+        Node[] nodes = new Node[nodesNumber + 1];
+        int n = 1;
+        while (n <= nodesNumber) {
+            nodes[n] = new Node(n);
+            n++;
+        }
+
+        FileReader newFileReader = new FileReader("text.txt");
+        BufferedReader newBufferedReader = new BufferedReader(newFileReader);
+
+        int index = 0;
+        for (int i = 1; i <= nodesNumber; i++) {
+            index++;
+            String lowNode = newBufferedReader.readLine();
+            for (int j = 0; j < lowNode.length(); j++) {
+                if (String.valueOf(lowNode.charAt(j)).equals(" ")) {
+                    j++;
+                }
+                nodes[index].addNode(nodes[Character.getNumericValue(lowNode.charAt(j))]);
+                nodes[Character.getNumericValue(lowNode.charAt(j))].addNode(nodes[index]);
+            }
+        }
+        BridgeFinder bridgeFinder = new BridgeFinder(nodes, bridges);
+        bridgeFinder.getResult();
+    }
 
     private Node[] nodes;
 
-    BridgeFinder(Node[] nodes, StringBuilder bridges) {
+    private BridgeFinder(Node[] nodes, StringBuilder bridges) {
         this.nodes = nodes;
     }
 
-    void getResult() {
-        for (int i = 1; i < nodes.length; i++) {
-            if (!nodes[i].isVisited()) {
-                DFS(1, i, -1);
-            }
-        }
-    }
+    private int DeepFirstSearch(int indexDFS, int nodeIndex, int parentNode) {
+        Node node = nodes[nodeIndex];
+        node.setLow(indexDFS);
+        node.setDfsIndex(indexDFS);
 
-    private int DFS(int indexDFS, int currentNodeNumber, int parentNode) {
-        Node currentNode = nodes[currentNodeNumber];
-        currentNode.setDfsIndex(indexDFS);
-        currentNode.setLow(indexDFS);
-
-        for (int i = 0; i < currentNode.getNodes().size(); i++) {
-            Node neighbour = currentNode.getNodes().get(i);
-            if (neighbour.getNodeIndex() != parentNode) {
-                if (!(neighbour.getDfsIndex() > 0)) {
-                    int nestedLow = DFS(indexDFS + 1, neighbour.getNodeIndex(), currentNodeNumber);
-                    if (nestedLow < currentNode.getLow()) {
-                        currentNode.setLow(nestedLow);
+        for (int i = 0; i < node.getNodes().size(); i++) {
+            Node neighbourNode = node.getNodes().get(i);
+            if (neighbourNode.getNodeIndex() != parentNode) {
+                if (neighbourNode.getDfsIndex() <= 0) {
+                    int nestedLow = DeepFirstSearch(indexDFS + 1, neighbourNode.getNodeIndex(), nodeIndex);
+                    if (nestedLow < node.getLow()) {
+                        node.setLow(nestedLow);
                     }
 
-                } else if (neighbour.getDfsIndex() < currentNode.getLow()) {
-                    currentNode.setLow(neighbour.getDfsIndex());
+                } else if (neighbourNode.getDfsIndex() < node.getLow()) {
+                    node.setLow(neighbourNode.getDfsIndex());
                 }
             }
         }
-
-
-        if (parentNode > -1 && currentNode.getDfsIndex() == currentNode.getLow()) {
-            System.out.println(parentNode + " " + currentNodeNumber);
+        if (parentNode > -1 && node.getDfsIndex() == node.getLow()) {
+            System.out.println(parentNode + " " + nodeIndex);
         }
-        return currentNode.getLow();
+        return node.getLow();
+    }
+
+    private void getResult() {
+        for (int i = 1; i < nodes.length; i++) {
+            if (!nodes[i].isVisited()) {
+                DeepFirstSearch(1, i, -1);
+            }
+        }
     }
 }
